58f2f124d714cb7bb61c26c70fbe7caa847a226c
Oskar Bohuszewicz-Słuszniak <oskarsluszniak@gmail.com>
2023-07-13 22:06:37 +0200

    created fake connecting to server

--- src/app/home/chat/chat-room/chat-room.component.ts
+++ src/app/home/chat/chat-room/chat-room.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PlayerService } from 'src/app/global-services/player.service';
 
 @Component({
   selector: 'app-chat-room',
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.scss'],
 })
-export class ChatRoomComponent {
-  
+export class ChatRoomComponent implements OnInit, OnDestroy {
+  isLogin: boolean = false;
+  token: string = '';
+  conecting: string = '';
+  conected: string = '';
+  show: boolean = false;
+
+  private subscription: Subscription = new Subscription();
+
+  constructor(private playerService: PlayerService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.playerService.isLogin$.subscribe((isLogin) => {
+      this.isLogin = isLogin;
+      this.fakeLoad();
+    });
+  }
+
+  fakeLoad() {
+    setTimeout(() => {
+      this.token = 'Generating authentication token...';
+      setTimeout(() => {
+        this.conecting = 'Connecting to server...';
+        setTimeout(() => {
+          this.conected = 'Conected!';
+          setTimeout(() => {
+            this.show = true;
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
